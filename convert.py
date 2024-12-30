7e03a76f655306784ae4165c5f4f338be8f26e8c
Jukka Suomela <jukka.suomela@iki.fi>
2024-12-30 03:00:52 +0200

    Convert to Rust code

---
+++ convert.py
@@ -0,0 +1,42 @@
+from contextlib import redirect_stdout
+
+def myrepr(c):
+    if c in ["'", "\\"]:
+        return f"'\\{c}'"
+    elif c.isprintable():
+        return f"'{c}'"
+    else:
+        return f"'\\u{{{ord(c):04X}}}'"
+
+def main():
+    xlat = []
+    with open('cp437.txt') as f:
+        for line in f:
+            line = line.rstrip()
+            if line.startswith('#') or line == '\x1a':
+                continue
+            cp437, unicode, comment = line.split('\t')
+            assert comment.startswith('#')
+            cp437 = int(cp437, base=0)
+            unicode = chr(int(unicode, base=0))
+            assert cp437 == len(xlat)
+            xlat.append(unicode)
+    print('pub const CP437: [char; 256] = [')
+    start = '   '
+    buf = None
+    for r in map(myrepr, xlat):
+        add = ' ' + r + ','
+        if buf is None:
+            buf = start
+        if len(buf + add) >= 100:
+            print(buf)
+            buf = start
+        buf = buf + add
+    if buf is not None:
+        print(buf)
+    print('];')
+
+if __name__ == "__main__":
+    with open('cp437.rs', 'w') as f:
+        with redirect_stdout(f):
+            main()
--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from cp437xlat!")
-
-
-if __name__ == "__main__":
-    main()
